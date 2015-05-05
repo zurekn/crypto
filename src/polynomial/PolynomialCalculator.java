9ebde6714b9ef71d2358a81da9be6241f32b173a
rulofg <sptg18@gmail.com>
2015-05-05 15:03:16 +0200

    Add calculator

---
+++ src/polynomial/PolynomialCalculator.java
@@ -0,0 +1,27 @@
+package polynomial;
+
+public abstract class PolynomialCalculator {
+	public static Polynomial add(Polynomial p, Polynomial q) {
+
+		return null;
+	}
+
+	public static Polynomial multiply(Polynomial p, Polynomial q) {
+
+		return null;
+	}
+
+	public static Polynomial substract(Polynomial p, Polynomial q) {
+
+		return null;
+	}
+
+	public static Polynomial pow(Polynomial p, int n) {
+
+		return null;
+	}
+	
+	public static boolean isPrimary(Polynomial p){
+		return false;
+	}
+}

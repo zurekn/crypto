e8c3225b8de7753a0aa1b54f710c3cfc4f84f1c9
rulofg <sptg18@gmail.com>
2015-05-05 15:07:30 +0200

    updated Polynomial class

--- src/polynomial/Polynomial.java
+++ src/polynomial/Polynomial.java
@@ -2,7 +2,7 @@ package polynomial;
 
 public class Polynomial {
 	private int[] polynomial = {0};
-	private int base;
+	private int basis;
 	
 	public Polynomial(){}
 	
@@ -10,17 +10,17 @@ public class Polynomial {
 		this.polynomial = p;
 	}
 	
-	public Polynomial(int[] p, int base){
+	public Polynomial(int[] p, int basis){
 		this(p);
-		this.base = base ;
+		this.basis = basis ;
 	}
 	
 	public int getDegree(){
 		return polynomial.length-1;
 	}
 	
-	public int getBase(){
-		return this.base ;
+	public int getBasis(){
+		return this.basis ;
 	}
 
 	

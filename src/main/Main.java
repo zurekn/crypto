bbb0191b86e9236d5606755fcee744b08f0d1af7
rulofg <sptg18@gmail.com>
2015-05-19 14:44:48 +0200

    Updated Polynomial

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		Polynomial p = new Polynomial(tab,2);
 		int[] tab2 = {1,1,0,1,1,1};
 		Polynomial q = new Polynomial(tab2,2);*/
-		int[] tab = {0,3,5,6,8,9};
+		/*int[] tab = {0,3,5,6,8,9};
 
 		Polynomial p = new Polynomial(tab);
 		int[] tab2 = {0,1,2,4,5};
@@ -20,8 +20,12 @@ public class Main {
 		System.out.println(p.toString());
 		System.out.println("%\n"+q.toString());
 		Polynomial res = PolynomialCalculator.mod(p, q);
-		System.out.println("=\n"+res.toString());
+		System.out.println("=\n"+res.toString());*/
+		
 		//System.out.println("\n---------------\n"+PolynomialCalculator.GCD(p, q).toString());
+		int[] toto ={0};
+		Polynomial p = new Polynomial(toto); 
+		System.out.println(p.getValue(0));
 		
 	}
 
